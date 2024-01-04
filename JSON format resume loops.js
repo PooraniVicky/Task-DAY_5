7ec8ec0efc22de7249ddf85ca84b7d60a5f89187
PooraniVicky <154896129+PooraniVicky@users.noreply.github.com>
2024-01-04 20:58:08 +0530

    Add files via upload

---
+++ JSON format resume loops.js
@@ -0,0 +1,51 @@
+//example resume for iterate:
+
+var json = [{
+    "id"  : "Poorani", 
+    "msg" : "For the given JSON iterate over all for loops (for, for in, for of, forEach)",
+    "mail": "poorani2475@gmail.com"
+},
+{
+    "id"  : "PooraniVicky", 
+    "msg" : "For the given JSON iterate over all for loops (for, for in, for of, forEach)",
+    "mail": "poorani2475@gmail.com"
+}];
+
+//--------------------------------------------------------------------------------------------
+
+//for loop
+for(var i = 0; i < json.length; i++) {
+    var obj = json[i];
+
+    console.log(obj.id);
+    console.log(obj.msg);
+    console.log(obj.mail);
+
+}
+//-----------------------------------------------------------------------------------------------
+
+//for Each
+json.forEach(function(obj) { console.log(obj.msg); });
+
+//------------------------------------------------------------------------------------------------
+
+//for In
+for (var key in json) {
+if (json.hasOwnProperty(key)) {
+  console.log(json[key].id);
+  //console.log(json[key].msg);
+ 
+}
+}
+
+
+//----------------------------------------------------------------------------------------------
+
+//for Of
+let text = "";
+for (let x of json[key].id) {
+ text += x; 
+}
+ console.log(text);
+
+//---------------------------------------------------------------------------------------------
---
+++ JSON format resume.js
@@ -0,0 +1,54 @@
+let myResume={
+    "basics": {
+      "name": "POORANI S",
+      "email": "poorani2475@gamil.com",
+      "phone": 8754276838,
+      "degree": "Diploma",
+      "location": {
+        "address": "No.10, 1st floor, shangapriya Apartment, Kilabakkam",
+        "postalCode": 600048,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India",
+        "CurrentCTC": 30000,
+        "ExpectedCTC": 40000
+       
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/poorani-s-57a358273",
+          "github":"https://github.com/PooraniVicky"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "Prabha Engineering Pvt Ltd",
+        "position": "Executive",
+        "startDate": "2023-04-01",
+            "summary": "i am the executive handling various task and gaining lots of experience in sales, Production planning control",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Arasan Ganesan Polytechnic College ",
+        "department": "ECE",
+        "studyType": "fulltime",
+        "batch start year": 2015,
+        "batch end year": 2018,
+        "Percentage": 83,
+      }
+    ],
+    "skills": [
+      {
+        "name": "python,javascript",
+        "level": "beginer",
+              }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+   }
+  console.log(myResume);
